cadfff20574cf61a72a5f87c210cd51615d55681
ikuchko <ikuchko@gmail.com>
2015-11-12 10:03:42 -0500

    make a function wich creates a sequence for the PingPong

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,21 @@
-var pingPong = function(number){
-  if (number % 3 === 0) {
-      return "ping";
-  } else if (number % 5 === 0) {
-    return "pong";
-  } else if (number % 15 === 0) {
-    return "pingpong"
-  } else return number;
+var pingPong = function(userNumber){
+  var pingPongNumber;
+  if (userNumber % 3 === 0 && userNumber % 5 === 0) {
+    pingPongNumber = "pingpong";
+  } else if (userNumber % 3 === 0) {
+      pingPongNumber = "ping";
+  } else if (userNumber % 5 === 0) {
+    pingPongNumber = "pong";
+  } else pingPongNumber = userNumber;
+  return pingPongNumber;
+
+
+}
+
+var sequenceMaker = function (userNumber) {
+  var sequenceOfNumbers = [];
+  for (var i = 1; i <= userNumber; i++ ) {
+    sequenceOfNumbers.push(pingPong(i));
+  }
+  return sequenceOfNumbers;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -16,6 +16,12 @@ describe("pingPong", function() {
   });
 
   it("number not divisible by 3 or 5 supposed to stay number", function() {
-    expect(pingPong(2)).to.equal(2)
+    expect(pingPong(2)).to.equal(2);
+  });
+});
+
+describe("sequenceMaker", function() {
+  it("chosen number must be equal to sequence of elements according rulls", function() {
+    expect(sequenceMaker(16)).to.eql([1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong", 11, "ping", 13, 14, "pingpong", 16]);
   });
 });
